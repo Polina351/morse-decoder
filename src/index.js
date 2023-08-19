efcd7827e7cedde838bb629f8e3513246797ba9e
Polina Deniskina <polinaoioi67@gmail.com>
2023-08-20 00:38:17 +0300

    feat: create task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    const countSymb = expr.length / 10;
+    const countSubStr = 10 / 2;
+    console.log(countSymb);
+    for (let i = 0; i < countSymb; i++) {
+        const subStr = expr.substring(i * 10, (i + 1) * 10);
+        console.log(subStr);
+        let subMorse = '';
+        for (let j = 0; j < subStr.length; j++) {
+            const symb = subStr.substring(j * 2, (j + 1) * 2);
+            if (symb === '00') {
+                continue;
+            } else if (symb === '10') {
+                subMorse += '.';
+            } else if (symb === '11') {
+                subMorse += '-';
+            } else {
+                subMorse += symb;
+            }
+        }
+        if (subMorse != '**********') {
+            res += MORSE_TABLE[subMorse]
+        } else {
+            res += ' ';
+        }
+    }
+    return res;
 }
 
 module.exports = {
